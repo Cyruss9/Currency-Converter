6298d84872ae102d90982f888ca1a3e3148ce23d
Cyruss9 <maniadit51@gmail.com>
2024-09-07 19:51:17 +0530

    Third commit

---
+++ app.js
@@ -0,0 +1,24 @@
+const BASE_URL = "https://cdn.jsdelivr.net/npm/@fawazahmed0/currency-api@latest/v1/currencies/eur.json"
+
+const dropdown = document.querySelectorAll(".dropdown select");
+
+for(let select of dropdown) {
+    for (currCode in countryList) {
+        let newOption = document.createElement("option");
+        newOption.innerText = currCode;
+        newOption.value = currCode;
+        if (select.name === "from" && currCode === "USD") {
+            newOption.selected = "selected";
+        } else {
+            if (select.name === "to" && currCode === "INR") {
+                newOption.selected = "selected";
+            }
+        }
+        select.append(newOption);
+    }
+
+    select.addEventListener("change", (evt) => {
+        updateFlag(evt.target);
+    });
+}
+
